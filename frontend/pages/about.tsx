7a4f32bb012517f8196a9e73cdd7895753d95228
geethaPsarathy <parthasarathy.g@northeastern.edu>
2022-11-07 09:25:08 -0500

    frontend next structure

---
+++ frontend/pages/about.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+const about = () =>{
+    return (
+        <div>
+          <h1>about</h1>
+          
+        </div>
+      )
+
+}
+
+export default about;
