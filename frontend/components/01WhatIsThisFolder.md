8dc9b44e8e59efcb063f342207e5b0d17cc1931d
parthasarathydNU <parthasarathy.d@northeastern.edu>
2022-11-12 23:11:48 -0500

    Adding layout to the UI

---
+++ frontend/components/01WhatIsThisFolder.md
@@ -0,0 +1,3 @@
+## Components
+
+Here we will be including layout components, header, navigation components so on so forth.
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,21 @@
+/**
+ * File Name: index.tsx
+ * Author: Dhruv
+ * File Created: Nov, 12 2022, Sat
+ * 
+ * About: 
+ * This is the entry point to the application
+ */
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import Layout from '../components/layout/layout'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hi Next JS APP</h1>
-      
-    </div>
+    <Layout>
+
+      Main content here
+
+    </Layout>
   )
 }
